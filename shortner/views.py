940f373699768bcecc218dca9e5a64a5c013bc9e
TheGiantNinja <yasserfedsi34@gmail.com>
2024-04-01 08:24:05 +0200

    first commit

---
+++ shortner/views.py
@@ -0,0 +1,20 @@
+import uuid
+from django.shortcuts import redirect, render
+from .models import Url
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def create(request):
+    if request.method == "POST":
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+    
+def go(request, pk):
+    url_details = Url.objects.create(uuid=pk)
+    return redirect('https://'+url_details.link)
